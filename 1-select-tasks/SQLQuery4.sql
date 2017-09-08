916f551b51dc6b17f0238236e0027b7bce954bb2
Sergey Dubovich <sergey.dubovich@aisnovations.com>
2017-09-08 19:02:50 +0300

    Eddited

--- 1-select-tasks/SQLQuery4.sql
+++ 1-select-tasks/SQLQuery4.sql
@@ -1,4 +1,4 @@
-SELECT [cus].[CustomerID], COUNT([ord].[OrderID]) AS 'Total number of Orders',  CAST(COUNT([ord].[OrderID]) AS DECIMAL) * 100 / (SELECT COUNT([dbo].[Orders].[OrderID]) FROM [dbo].[Orders] )  AS '% of all orders'
+SELECT [cus].[CustomerID], COUNT(*) AS [Total number of Orders],  CAST( COUNT(*) AS DECIMAL) * 100 / (SELECT COUNT(*) FROM [dbo].[Orders] )  AS [% of all orders]
 FROM [dbo].[Customers] [cus]
 JOIN [dbo].[Orders] [ord] ON [cus].[CustomerID] = [ord].[CustomerID]
 GROUP BY [cus].[CustomerID] 
