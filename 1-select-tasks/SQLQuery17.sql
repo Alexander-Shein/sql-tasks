3f4de9cd7cab410bb1581410fdfc6e763e29c32a
Sergey Dubovich <sergey.dubovich@aisnovations.com>
2017-09-08 19:30:17 +0300

    Eddited

--- 1-select-tasks/SQLQuery17.sql
+++ 1-select-tasks/SQLQuery17.sql
@@ -1,4 +1,4 @@
-SELECT [c].[CategoryName], AVG([p].UnitPrice) AS 'AvgPrice' 
+SELECT [c].[CategoryName], AVG( [p].[UnitPrice] ) AS [AvgPrice] 
 FROM [dbo].[Categories] [c]
-JOIN [dbo].[Products] [p] ON [c].CategoryID = [p].[CategoryID]
+JOIN [dbo].[Products] [p] ON [c].[CategoryID] = [p].[CategoryID]
 GROUP BY [c].[CategoryName]
