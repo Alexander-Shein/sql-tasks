1011d6a16f2580e4eba1ddfcdfed00569732e537
Sergey Dubovich <sergey.dubovich@aisnovations.com>
2017-09-08 19:12:13 +0300

    Eddited code style

--- 1-select-tasks/SQLQuery7.sql
+++ 1-select-tasks/SQLQuery7.sql
@@ -1,4 +1,4 @@
-SELECT e.EmployeeID, e.FirstName + ' ' + e.LastName AS 'FullName',  r.FirstName + ' ' + r.LastName AS 'ReportsTo'
-FROM dbo.Employees [e]
-LEFT JOIN dbo.Employees [r] ON e.ReportsTo = r.EmployeeID
-ORDER BY e.EmployeeID
+SELECT [e].[EmployeeID], [e].[FirstName] + ' ' + [e].[LastName] AS [FullName],  [r].[FirstName] + ' ' + [r].[LastName] AS [ReportsTo]
+FROM [dbo].[Employees] [e]
+LEFT JOIN [dbo].[Employees] [r] ON [e].[ReportsTo] = [r].[EmployeeID]
+ORDER BY [e].[EmployeeID]
