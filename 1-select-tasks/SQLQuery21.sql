6602f747efcdfc27d98bf080894fa41d71caecd1
Sergey Dubovich <sergey.dubovich@aisnovations.com>
2017-09-08 19:34:16 +0300

    Eddited

--- 1-select-tasks/SQLQuery21.sql
+++ 1-select-tasks/SQLQuery21.sql
@@ -1,3 +1,3 @@
-SELECT [od].[OrderID], MAX([od].[Quantity] * [od].[UnitPrice]) AS [Maximum Purchase Amount, $]
+SELECT [od].[OrderID], MAX( [od].[Quantity] * [od].[UnitPrice] ) AS [Maximum Purchase Amount, $]
 FROM [dbo].[Order Details] [od]
 GROUP BY [od].[OrderID]
