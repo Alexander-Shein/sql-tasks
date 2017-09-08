77d62b028a63fcfbba465d4b45a223a2bbf32503
Sergey Dubovich <sergey.dubovich@aisnovations.com>
2017-09-08 19:07:19 +0300

    Eddited

--- 1-select-tasks/SQLQuery6.sql
+++ 1-select-tasks/SQLQuery6.sql
@@ -1,4 +1,4 @@
-SELECT [p].[ProductName], [c].[CategoryName], [s].[CompanyName] AS 'SupplierCompanyName'
+SELECT [p].[ProductName], [c].[CategoryName], [s].[CompanyName] AS [SupplierCompanyName]
 FROM [dbo].[Products] [p]
 JOIN [dbo].[Categories] [c] ON [p].[CategoryID] = [c].[CategoryID]
 JOIN [dbo].[Suppliers] [s] ON [p].[SupplierID] = [s].[SupplierID]
