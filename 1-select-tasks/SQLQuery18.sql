24acca137def25cb3ee10fb2e45d834a00a4c6e7
Sergey Dubovich <sergey.dubovich@aisnovations.com>
2017-09-11 10:19:48 +0300

    Eddited with YEAR function

--- 1-select-tasks/SQLQuery18.sql
+++ 1-select-tasks/SQLQuery18.sql
@@ -1,4 +1,4 @@
 SELECT [o].[OrderDate], COUNT(*) AS [Total Number of Orders] 
 FROM [dbo].[Orders] [o]
-WHERE [o].[OrderDate] BETWEEN '1998-01-01' AND '1998-12-31'
+WHERE YEAR([o].[OrderDate]) = 1998 
 GROUP BY [o].[OrderDate]
