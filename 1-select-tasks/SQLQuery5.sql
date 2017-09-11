f5a108ae4da84230edca0c9081b44a6bba98a3b5
Sergey Dubovich <sergey.dubovich@aisnovations.com>
2017-09-11 09:32:45 +0300

    Edited to shorter LIKE pattern

--- 1-select-tasks/SQLQuery5.sql
+++ 1-select-tasks/SQLQuery5.sql
@@ -1,4 +1,4 @@
 SELECT [p].[ProductID], [p].[ProductName], [p].[QuantityPerUnit] 
 FROM [dbo].[Products] [p]
-WHERE [p].[ProductName] LIKE '[A,B,C,D,E,F]%'
+WHERE [p].[ProductName] LIKE '[A-F]%'
 ORDER BY [p].[ProductName]
