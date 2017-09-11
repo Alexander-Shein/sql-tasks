070d1aaa31830439b6fcbe2abee2e1f239bd6d90
Sergey Dubovich <sergey.dubovich@aisnovations.com>
2017-09-11 11:05:50 +0300

    Rewrited query with GROUP BY and HAVING

--- 1-select-tasks/SQLQuery19.sql
+++ 1-select-tasks/SQLQuery19.sql
@@ -1,17 +1,6 @@
-SELECT [t2].[CustomerID], [Customers].[CompanyName], [t2].[TotalOrdersAmount, $]
-FROM 
-	(	SELECT [Customers].[CustomerID], SUM([t1].[AMOUNT]) AS [TotalOrdersAmount, $] 
-		FROM 
-		(SELECT [od].[OrderID], SUM([od].[UnitPrice] * [od].[Quantity]) AS [AMOUNT]
-		 FROM [Order Details] [od]
-		 JOIN  [Orders] [o] ON [od].[OrderID] = [o].[OrderID] 
-		 GROUP BY [od].[OrderID] 
-		) [t1]
-
-	JOIN [Orders] ON [t1].[OrderID] = [Orders].[OrderID]
-	JOIN [Customers] ON [Orders].[CustomerID] = [Customers].[CustomerID]
-	GROUP BY [Customers].[CustomerID] 
-	) [t2]
-
-JOIN [Customers] ON [t2].[CustomerID] = [Customers].[CustomerID]
-WHERE [t2].[TotalOrdersAmount, $] > 10000
+SELECT [c].[CustomerID], [c].[CompanyName], SUM([od].[UnitPrice] * [od].[Quantity]) AS [TotalOrdersAmount, $]
+FROM [dbo].[Customers] [c]
+JOIN [dbo].[Orders] [o] ON [c].[CustomerID] = [o].[CustomerID]
+JOIN [dbo].[Order Details] [od] ON [o].[OrderID] = [od].[OrderID]
+GROUP BY [c].[CustomerID], [c].[CompanyName]
+HAVING SUM([od].[UnitPrice] * [od].[Quantity]) > 10000
