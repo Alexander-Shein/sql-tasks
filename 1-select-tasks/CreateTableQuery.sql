cfc7e5bf271763d587d41bbda13b88b805acc0a8
Sergey Dubovich <sergey.dubovich@aisnovations.com>
2017-09-11 13:53:32 +0300

    RepeatableRead

---
+++ 1-select-tasks/CreateTableQuery.sql
@@ -0,0 +1,8 @@
+DROP TABLE IF exists RepetableReadDemo;
+CREATE TABLE RepetableReadDemo (
+	Id INT,
+	Name VARCHAR(20)
+);
+INSERT INTO RepetableReadDemo (Id, Name) VALUES (1,'Frist'),(2,'Second');
+
+
