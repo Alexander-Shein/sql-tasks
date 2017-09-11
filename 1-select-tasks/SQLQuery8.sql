5af3e7759d89ed786e80176a9c3c6ce0279712d0
Sergey Dubovich <sergey.dubovich@aisnovations.com>
2017-09-11 09:49:36 +0300

    Rewrited task

--- 1-select-tasks/SQLQuery8.sql
+++ 1-select-tasks/SQLQuery8.sql
@@ -1,4 +1,4 @@
-SELECT [c].[CategoryName], SUM( [p].[UnitsInStock] + [p].[UnitsOnOrder] ) AS [TotalNumberOfProducts]
+SELECT [c].[CategoryName], COUNT(*) AS [TotalNumberOfProducts]
 FROM [dbo].[Categories] [c]
 LEFT JOIN [dbo].[Products] [p] ON [c].[CategoryID] = [p].[CategoryID]
 GROUP BY [c].[CategoryName]
