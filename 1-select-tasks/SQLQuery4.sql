6fc741b44f3a50dac58a0b82ba38caf539ebc838
Sergey Dubovich <sergey.dubovich@aisnovations.com>
2017-09-11 09:31:17 +0300

    Removed Join

--- 1-select-tasks/SQLQuery4.sql
+++ 1-select-tasks/SQLQuery4.sql
@@ -1,5 +1,4 @@
-SELECT [cus].[CustomerID], COUNT(*) AS [Total number of Orders],  CAST( COUNT(*) AS DECIMAL) * 100 / (SELECT COUNT(*) FROM [dbo].[Orders] )  AS [% of all orders]
-FROM [dbo].[Customers] [cus]
-JOIN [dbo].[Orders] [ord] ON [cus].[CustomerID] = [ord].[CustomerID]
-GROUP BY [cus].[CustomerID] 
+SELECT [ord].[CustomerID] , COUNT(*) AS [Total number of Orders],  CAST( COUNT(*) AS DECIMAL) * 100 / (SELECT COUNT(*) FROM  [Northwind].[dbo].[Orders] )  AS [% of all orders]
+FROM [Northwind].[dbo].[Orders] [ord]
+GROUP BY [ord].[CustomerID] 
 ORDER BY [% of all orders] DESC
