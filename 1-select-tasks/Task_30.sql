3a2208674368718935e3ce82953264242c6936f1
Slava Peshko <slava.peshko@gmail.com>
2017-09-11 17:15:52 +0300

    Task_30

---
+++ 1-select-tasks/Task_30.sql
@@ -0,0 +1,13 @@
+﻿USE Northwind
+GO
+
+-- Создать хранимую процедуру которая принимает OrderId возвращает Order и Order Details двумя разными селектами. (GetOrderById)
+GO
+CREATE PROCEDURE GetOrderById 
+@Id INTEGER
+AS
+		SELECT*FROM [dbo].[Orders] WHERE [OrderID] = @Id
+		SELECT*FROM [dbo].[Order Details] WHERE [OrderID] = @Id
+GO
+
+EXECUTE GetOrderById 10251
