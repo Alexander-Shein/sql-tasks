f1fa05ebab127110f04a2b34d52e4694f04f9e15
Slava Peshko <slava.peshko@gmail.com>
2017-09-11 17:43:25 +0300

    +added task_31

---
+++ 1-select-tasks/Task31.sql
@@ -0,0 +1,27 @@
+﻿--Создать хранимую процедуру которая принимает OrderID удаляет Order и Order Details по этому ID. (DeleteOrderById)
+
+USE [Northwind]
+GO
+
+GO
+CREATE PROCEDURE DeleteOrderById(@OrderID  INTEGER)
+	AS
+		BEGIN TRAN
+
+			BEGIN TRY
+				DELETE FROM [Order Details]
+					WHERE [OrderID] = @OrderID
+	
+				DELETE FROM [Orders]
+					WHERE [OrderID] = @OrderID
+			END TRY
+
+			BEGIN CATCH
+				PRINT 'Error'
+				ROLLBACK COMMIT
+			END CATCH
+
+		COMMIT TRANSACTION
+GO 
+
+EXECUTE DeleteOrderById 10251
