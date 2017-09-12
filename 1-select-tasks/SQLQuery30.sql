754c99619f3f0126f0d76fa4e49e2c4612e37ab6
Sergey Dubovich <sergey.dubovich@aisnovations.com>
2017-09-12 10:14:08 +0300

    Replaced * in procedure

--- 1-select-tasks/SQLQuery30.sql
+++ 1-select-tasks/SQLQuery30.sql
@@ -4,5 +4,25 @@ CREATE OR ALTER PROCEDURE [dbo].[GetOrderById]
 @OrderID INT
 AS   
 	SET NOCOUNT ON;
-	SELECT * FROM [dbo].[Orders] [o] WHERE [o].[OrderID] = @OrderID;
-	SELECT * FROM [dbo].[Order Details] [od] WHERE [od].[OrderID] = @OrderID;
+	SELECT	[o].[OrderID],
+			[o].[CustomerID],
+			[o].[EmployeeID],
+			[o].[OrderDate],
+			[o].[RequiredDate],
+			[o].[ShippedDate],
+			[o].[ShipVia],
+			[o].[Freight],
+			[o].[ShipName],
+			[o].[ShipAddress],
+			[o].[ShipCity],
+			[o].[ShipRegion],
+			[o].[ShipPostalCode],
+			[o].[ShipCountry] 
+	 FROM [dbo].[Orders] [o] WHERE [o].[OrderID] = @OrderID;
+	
+	SELECT	[od].[OrderID],
+			[od].[ProductID],
+			[od].[UnitPrice],
+			[od].[Quantity],
+			[od].[Discount]
+	 FROM [dbo].[Order Details] [od] WHERE [od].[OrderID] = @OrderID;
