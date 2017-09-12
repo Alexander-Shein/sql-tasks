2abfcbe5e325e95bbb41e4b3553b7ab07627902b
Andrey Likhtar <andrey.likhar@gmail.com>
2017-09-12 09:39:17 +0300

    Task 30 - Get rid of '*' operator

--- 1-select-tasks/task30_GetOrderById.sql
+++ 1-select-tasks/task30_GetOrderById.sql
@@ -1,9 +1,30 @@
-CREATE PROCEDURE GetOrderById
+CREATE PROCEDURE [dbo].[GetOrderById]
 	@OrderId INT
 
 AS
 
-	SELECT * FROM [dbo].[Orders] [o]
+	SELECT [o].[OrderID]
+      ,[o].[CustomerID]
+      ,[o].[EmployeeID]
+      ,[o].[OrderDate]
+      ,[o].[RequiredDate]
+      ,[o].[ShippedDate]
+      ,[o].[ShipVia]
+      ,[o].[Freight]
+      ,[o].[ShipName]
+      ,[o].[ShipAddress]
+      ,[o].[ShipCity]
+      ,[o].[ShipRegion]
+      ,[o].[ShipPostalCode]
+      ,[o].[ShipCountry]
+	   
+	FROM [dbo].[Orders] [o]
 	WHERE [o].[OrderID] = @OrderId;
-	SELECT * FROM [dbo].[Order Details] [od]
+	SELECT [od].[OrderID]
+      ,[od].[ProductID]
+      ,[od].[UnitPrice]
+      ,[od].[Quantity]
+      ,[od].[Discount] 
+	
+	FROM [dbo].[Order Details] [od]
 	WHERE [od].[OrderID] = @OrderId;
