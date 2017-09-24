ef6851acc5109e3e638de5b97f602db05a21901b
alekseyTask <AWork129@yandex.by>
2017-09-24 23:20:05 +0300

    31_3

--- 1-select-tasks/31_DeleteOrderById.sql
+++ 1-select-tasks/31_DeleteOrderById.sql
@@ -1,9 +1,9 @@
-
 /*
 ** Создать хранимую процедуру которая принимает OrderID удаляет Order и Order Details по этому ID. (DeleteOrderById)
+
 */
 
 CREATE PROCEDURE [dbo].[DeleteOrderById] (@OrderId INT) 
 AS
-DELETE FROM [dbo].[Orders][o] WHERE [o][OrderID]=@OrderId;
-DELETE FROM [dbo].[Order Details][OD] WHERE [OD].[OrderID]=@OrderId;
+DELETE FROM [dbo].[Order Details] WHERE [Order Details].[OrderId]=@OrderId;
+DELETE FROM [dbo].[Orders] WHERE [OrderId]=@OrderId;
