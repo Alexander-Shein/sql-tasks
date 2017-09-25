7c31bdf184d767da8343d2ed526b0460d2f7fdde
Сергей <limonad-vafel@list.ru>
2017-09-25 20:41:15 +0300

    PHANTOM READ

---
+++ 1-select-tasks/29.sql
@@ -0,0 +1,35 @@
+drop table if exists "Table"
+;
+create table "Table"
+(
+	Id int primary key not null,
+	Value int not null
+)
+;
+
+insert into "Table" values (1,1)
+;
+
+
+set transaction isolation level repeatable read
+;
+
+begin	
+;
+	select * from "Table"
+	;
+	select pg_sleep(2)
+	;
+	select * from "Table"
+	;
+	
+commit
+;
+
+begin	
+;
+	insert into "Table" values (2,100)
+	;
+	
+commit
+;
