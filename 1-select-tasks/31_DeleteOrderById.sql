2e6aa8b0a9540210c0ffb9da0624c5ca07c221ca
alesha <work04092017@gmail.com>
2017-10-10 18:30:07 +0300

    31_5

--- 1-select-tasks/31_DeleteOrderById.sql
+++ 1-select-tasks/31_DeleteOrderById.sql
@@ -5,7 +5,12 @@
 
 CREATE PROCEDURE [dbo].[DeleteOrderById] (@OrderId INT) 
 AS
-BEGIN TRANSACTION;
-	DELETE FROM [dbo].[Order Details] WHERE [Order Details].[OrderId]=@OrderId;
-	DELETE FROM [dbo].[Orders] WHERE [OrderId]=@OrderId;
-COMMIT TRANSACTION;
+BEGIN TRY
+	BEGIN TRANSACTION;
+		DELETE FROM [dbo].[Order Details] WHERE [Order Details].[OrderId]=@OrderId;
+		DELETE FROM [dbo].[Orders] WHERE [OrderId]=@OrderId;
+	COMMIT TRANSACTION;
+END TRY 
+BEGIN CATCH
+	ROLLBACK ;
+END CATCH
